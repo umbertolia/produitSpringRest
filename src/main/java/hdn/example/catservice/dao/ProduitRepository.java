109f62e309d0f52cc86c6017aa4f7900f9b2c853
umbertolia <bertik@sfr.fr>
2020-03-01 17:12:14 +0100

    MAJ produitSpringRestServer

--- src/main/java/hdn/example/catservice/dao/ProduitRepository.java
+++ src/main/java/hdn/example/catservice/dao/ProduitRepository.java
@@ -8,9 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import hdn.example.catservice.entities.Produit;
 
+@CrossOrigin(origins = "*")
 @RepositoryRestResource(path = "produits")
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
 
